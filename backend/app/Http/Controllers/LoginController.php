dacd9fe3ccf16bd8ea0fb3de31858e3504fc49c3
Mahmut İÇME <arrovain@gmail.com>
2024-07-02 11:36:31 +0300

    request for phone number and it should be numeric and min 10 character

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function submit(Request $request) {
+        $request->validate([
+            'phone' => 'required|numeric|min:10'
+    ])
+
+
+
+    }
 }
