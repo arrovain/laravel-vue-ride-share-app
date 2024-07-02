b12a2d26fac7c26aba72c1c479c83fb5129765ac
Mahmut İÇME <arrovain@gmail.com>
2024-07-02 10:50:28 +0300

    database changes

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('phone')->unique();
+            $table->string('login_code')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
