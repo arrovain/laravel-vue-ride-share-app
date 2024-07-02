c727e55d0c69e873d158d15c16f945fa7a89aefc
Mahmut İÇME <arrovain@gmail.com>
2024-07-02 11:01:29 +0300

    trips needs user and driver

--- backend/database/migrations/2024_07_02_075321_create_trips_table.php
+++ backend/database/migrations/2024_07_02_075321_create_trips_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('trips', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Driver::class);
             $table->timestamps();
         });
     }
