21b38291d2ab3807623e419f4e3982423b944a5a
Mahmut İÇME <arrovain@gmail.com>
2024-07-02 13:17:04 +0300

    create verify

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -26,9 +26,31 @@ class LoginController extends Controller
 
         return response()->json(['message' => 'Text message notification sent.']);
     }
+    public function verify(Request $request)
+    {
+        // validate the incoming request
+        $request->validate([
+            'phone' => 'required|numeric|min:10',
+            'login_code' => 'required|numeric|between:111111,999999'
+        ]);
+
+        // find the user
+        $user = User::where('phone', $request->phone)
+            ->where('login_code', $request->login_code)
+            ->first();
+
+        // is the code provided the same one saved?
+        // if so, return back an auth token
+        if ($user) {
+            $user->update([
+                'login_code' => null
+            ]);
+            
+            return $user->createToken($request->login_code)->plainTextToken;
+        }
 
 
 
 
-    }
+    };
 }
