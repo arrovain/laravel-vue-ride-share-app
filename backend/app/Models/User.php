2cb7580e50dda72414c963f81e3870586d69a6cb
Mahmut İÇME <arrovain@gmail.com>
2024-07-02 16:23:32 +0300

    guarded instead of fillable and loginCode

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -16,11 +16,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -28,7 +24,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
+        'login_code',
         'remember_token',
     ];
     public function routeNotificationForTwilio()
