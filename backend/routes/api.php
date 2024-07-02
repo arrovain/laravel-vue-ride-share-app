dc652fd156f596f00baacf369247402a1b54e777
Mahmut İÇME <arrovain@gmail.com>
2024-07-02 11:31:55 +0300

    array needs LoginController and a name

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', []);
+Route::post('/login', [LoginController::class, 'submit']);
